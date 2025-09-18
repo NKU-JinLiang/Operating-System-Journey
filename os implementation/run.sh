a6b6e37d4c65f14c0c6477ad4cb02a75022621da
unknown <1120230037@mai.nankai.edu.cn>
2025-09-18 22:42:21 +0800

    test

--- os implementation/run.sh
+++ os implementation/run.sh
@@ -2,5 +2,5 @@ nasm -f bin mbr.S -o mbr.bin
 nasm -f bin loader.S -o loader.bin
 dd if=/dev/zero of=os.img bs=512 count=100
 dd if=mbr.bin of=os.img conv=notrunc
-dd if=loader.bin of=os.img bs=512 count=2 seek=2 conv=notrunc
+dd if=loader.bin of=os.img bs=512 seek=2 conv=notrunc
 qemu-system-i386 -drive format=raw,file=os.img
